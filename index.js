e1d3b710d0e16748b86ab5957a12956d5d967273
MrAMB312 <a.blomarz@gmail.com>
2024-11-28 08:44:29 -0600

    complete phase 1 context lab

--- index.js
+++ index.js
@@ -1,5 +1,65 @@
 /* Your Code Here */
 
+function createEmployeeRecord(employeeArray) {
+    return {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+
+function createEmployeeRecords(arrayOfEmployees) {
+    return arrayOfEmployees.map(createEmployeeRecord);
+}
+
+function createTimeInEvent(dateStamp) {
+    const [date, time] = dateStamp.split(" ");
+    const timeInEvent = {
+        type: "TimeIn",
+        hour: parseInt(time),
+        date: date,
+    }
+    this.timeInEvents.push(timeInEvent);
+    return this;
+}
+
+function createTimeOutEvent(dateStamp) {
+    const [date, time] = dateStamp.split(" ");
+    const timeOutEvent = {
+        type: "TimeOut",
+        hour: parseInt(time),
+        date: date,
+    }
+    this.timeOutEvents.push(timeOutEvent);
+    return this;
+}
+
+function hoursWorkedOnDate(date) {
+    const timeInEvent = this.timeInEvents.find(event => event.date === date);
+    const timeOutEvent = this.timeOutEvents.find(event => event.date === date);
+    const hoursWorked = (timeOutEvent.hour - timeInEvent.hour) / 100;
+    return hoursWorked;
+}
+
+function wagesEarnedOnDate(date) {
+    const hoursWorked = hoursWorkedOnDate.call(this, date);
+    const wagesEarned = hoursWorked * this.payPerHour;
+    return wagesEarned;
+}
+
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(employeeRecord => employeeRecord.firstName === firstName)
+}
+
+function calculatePayroll(arrayOfEmployees) {
+    return arrayOfEmployees.reduce(function (totalPayroll, employee) {
+        return totalPayroll + allWagesFor.call(employee);
+    }, 0);
+}
+
 /*
  We're giving you this function. Take a look at it, you might see some usage
  that's new and different. That's because we're avoiding a well-known, but
